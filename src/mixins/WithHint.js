382086844322babdb7f4693288faed4067413128
Ralf86 <levenson94@gmail.com>
2016-01-18 17:14:06 +0300

    Translate HintMixin to higher order component

--- src/mixins/WithHint.js
+++ src/mixins/WithHint.js
@@ -1,17 +1,18 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Hint from '../components/Hint'
 
-export default {
-    getInitialState() {
-        return {
+export var WithHint = ComposedComponent => class extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
             x: undefined,
             y: undefined,
             text: undefined
-        }
-    },
+        };
+    }
     hideHint() {
         this.setState({ text: undefined })
-    },
+    }
     showHint(text) {
         return (ev) => {
             const { left, bottom, width } = ev.target.getBoundingClientRect()
@@ -20,8 +21,17 @@ export default {
 
             this.setState({ text, x, y })
         }
-    },
-    getHint() {
-        return this.state.text ? <Hint {...this.state} /> : null
     }
-}
+    getHint() {
+        return this.state && this.state.text ? <Hint {...this.state} /> : null
+    }
+    render() {
+        return <ComposedComponent
+                      hideHint={this.hideHint}
+                      showHint={this.showHint}
+                      getHint={this.getHint}
+                      {...this.props}
+                      {...this.state}
+                />;
+    }
+};
